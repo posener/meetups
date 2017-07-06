709877f779d08af8e6c744488505bf1731066791
Eyal Posener <posener@gmail.com>
2017-07-06 09:55:44 +0300

    fix handler function

--- websocket-testing/handler/handler.go
+++ websocket-testing/handler/handler.go
@@ -6,9 +6,8 @@ import (
 )
 
 func main() {
-	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>I love %s!</h1>", r.URL.Path[1:])
 	})
-	http.HandleFunc("/", h)
 	http.ListenAndServe(":8080", nil)
 }
